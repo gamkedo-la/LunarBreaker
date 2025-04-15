fad24a31815b27e034a47a839d8c3fa567cd4366
Chris DeLeon <chris@gamkedo.com>
2025-04-14 20:09:23 -0400

    Drone no longer needs debugsphere, basic distance strafe movement added

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,32 +5,43 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
 
-    public float moveSpeed;
     public Rigidbody rigidbody;
     private bool chasing;
-    public float distanceToChase = 10f, distanceToLose = 15f;
+    private float distanceToStop = 10f, distanceToChase = 25f, distanceToLose = 40f;
     private Vector3 targetPoint;
-    public Transform debugSphere;
-
+    public Transform playerTransform;
+    private bool strafeCW = false;
+    private float moveSpeed = 6.0f;
+    private float strafeSpeed = 5.0f;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerTransform = GameObject.Find("Player").transform;
+        StartCoroutine(SwitchStrafeDir());
+    }
+
+    IEnumerator SwitchStrafeDir()
+    {
+        while(true)
+        {
+            strafeCW = !strafeCW;
+            yield return new WaitForSeconds( Random.RandomRange(1.0f,3.0f) );
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        targetPoint = debugSphere.transform.position;
+        targetPoint = playerTransform.position;
         targetPoint.y = transform.position.y;
 
         if (!chasing)
         {
-            if(Vector3.Distance(transform.position,targetPoint) < distanceToChase)
+            if (Vector3.Distance(transform.position, targetPoint) < distanceToChase)
             {
                 chasing = true;
             }
@@ -40,8 +51,13 @@ public class EnemyController : MonoBehaviour
 
             transform.LookAt(targetPoint);
 
-
-            rigidbody.velocity = transform.forward * moveSpeed;
+            if (Vector3.Distance(transform.position, targetPoint) > distanceToStop)
+            {
+                rigidbody.velocity = transform.forward * moveSpeed;
+            } else
+            {
+                rigidbody.velocity = (strafeCW ? -1.0f : 1.0f)*transform.right * strafeSpeed;
+            }
 
             if(Vector3.Distance(transform.position,targetPoint) > distanceToLose)
             {
