2f15ef8e775d86e335c2f7a1c5761642fdbde0f1
IanCher <IanCher@users.noreply.github.com>
2025-04-23 09:15:02 +0200

    Make damage feedback fade in after some time

--- Assets/Scripts/DamageVisualFeedback.cs
+++ Assets/Scripts/DamageVisualFeedback.cs
@@ -1,30 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DamageVisualFeedback : MonoBehaviour
 {
-    [SerializeField] float alpha = 0.5f;
-    [SerializeField] float timeBeforeFadeOut = 0.5f;
+    [SerializeField] float alphaWhenVisisble = 0.5f;
+    [SerializeField] float timeBeforeFadeIn = 0.5f;
+    [SerializeField] float timeToFade = 1.0f;
+
     Image image;
+    bool isFading = false;
+    float fadeInRate;
 
     void Awake()
     {
         image = GetComponent<Image>();
     }
 
+    void Start()
+    {
+        isFading = false;
+        fadeInRate = alphaWhenVisisble / timeToFade;
+    }
+
+    void Update()
+    {
+        if (!isFading) return;
+
+        Color color = image.color;
+
+        image.color = new Color(
+            color.r,
+            color.g,
+            color.b,
+            Mathf.Clamp(color.a - fadeInRate * Time.deltaTime, 0, 1)
+        );
+
+        if (image.color.a == 0.0f)
+        {
+            isFading = false;
+        }
+    }
+
     public void ShowImage()
     {
         Color color = image.color;
-        image.color = new Color(color.r, color.g, color.b, alpha);
+        image.color = new Color(color.r, color.g, color.b, alphaWhenVisisble);
         StartCoroutine(FadeAfterTime());
     }
 
     private IEnumerator FadeAfterTime()
     {
-        yield return new WaitForSeconds(timeBeforeFadeOut);
-        Color color = image.color;
-        image.color = new Color(color.r, color.g, color.b, 0.0f);
+        yield return new WaitForSeconds(timeBeforeFadeIn);
+        isFading = true;
     }
 }
